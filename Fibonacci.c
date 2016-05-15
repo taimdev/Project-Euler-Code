e4f176b1f83c613a59ae805fcc32ffd369b84aa6
taimurazhar97 <taimurazhar97@gmail.com>
2016-05-16 00:42:20 +1000

    Project Euler Problem 2

---
+++ Fibonacci.c
@@ -0,0 +1,34 @@
+// Taimur Azhar z5116684
+// 02/05/2016
+// Project Euler Problems
+// Problem2 : Even Fibonacci Numbers
+// Sum of all the even Fibonacci numbers below 4,000,000
+// Solution : 4613732
+
+#include <stdio.h>
+#include <stdlib.h>
+ 
+#define LIMIT 4000000
+
+int main (int argc, char const *argv[]) {
+   int number = 2;
+   int prenumber = 1;
+   int store = 0;
+   int sum = 0;
+
+   while (number < LIMIT) {
+   	//printf("%d\n", number);
+
+       if (number % 2 == 0) {
+          sum += number;
+        }
+
+      store = number + prenumber;
+      prenumber = number;
+      number = store;
+
+   }
+   printf("Sum is %d\n", sum);
+
+   return EXIT_SUCCESS;
+}
